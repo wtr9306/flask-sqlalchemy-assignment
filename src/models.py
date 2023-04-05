fca932f6bc8b6a201fb13dfc6cc5393311e38e0e
wtr9306 <113145696+wtr9306@users.noreply.github.com>
2023-04-04 21:28:51 -0400

    Update models.py

--- src/models.py
+++ src/models.py
@@ -1,1 +1,19 @@
 # TODO - Create SQLAlchemy DB and Movie model
+from flask_sqlalchemy import SQLAlchemy
+
+# Create SQLAlchemy DB object
+db = SQLAlchemy()
+
+# Define Movie model
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    director = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    genre = db.Column(db.String(255), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
+    def __repr__(self):
+        return f"<Movie {self.id}: {self.title} ({self.year})>"
